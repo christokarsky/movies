04602eff99904638689b0984a41dbda7a97c2067
christokarsky <christokarsky1@gmail.com>
2023-07-12 10:45:13 -0500

    error log

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors")
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
-const notFound = require("./errors/notFound");
+const notFound = require("./errors/notFOund");
 const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
