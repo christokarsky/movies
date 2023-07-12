4546a38c19fe4aea0b849543e8a68431dd50d6e1
christokarsky <christokarsky1@gmail.com>
2023-07-12 10:56:49 -0500

    cors all

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
@@ -11,6 +12,7 @@ const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/theaters", theatersRouter);
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
